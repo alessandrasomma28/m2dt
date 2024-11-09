2c4b2c62356c62bc784fe86a73eb03f48bb5c120
Alessandra Somma <alessandra.somma@unina.it>
2024-11-09 09:04:34 +0100

    update

---
+++ MDAModelingLevels/04.ISM/Planner.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import ScenarioGenerator
+from typing import List
+
+class Planner(object):
+	def __init__(self):
+		self._unnamed_ScenarioGenerator_ : ScenarioGenerator = None
+		"""# @AssociationMultiplicity 1
+		# @AssociationKind Aggregation"""
+
